4b771058dc8a20857ce8574c001a78a492be8541
adv8750940073-beep <adv8750940073@gmail.com>
2026-01-01 13:40:41 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,11 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY!,
-  authDomain: "xxx.firebaseapp.com",
-  projectId: "xxx",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PID!,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
+export const auth = getAuth(app);
